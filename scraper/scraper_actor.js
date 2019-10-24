865db08fa17535c9e4cccea234c0f7371ede31b8
decoejz <decoejz@gmail.com>
2019-10-24 10:34:09 -0300

    scraper

--- scraper/scraper_actor.js
+++ scraper/scraper_actor.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const { chunk, linspace } = require("./util");
 var item;
 
 classmem = JSON.parse(fs.readFileSync("to_scrape.json", "utf8"));
@@ -17,25 +18,48 @@ const parseLinks = str => {
 
 //   const listActor = doc("table.cast_list tr[class] td:not([class])");
   const origin = doc("table#overviewTable.dataTable.labelValueTable td>a").text();
-    // console.log(nome,origin);
-    arr.push({
-        nome,origin
-    })
+  // console.log(nome,origin);
+  arr.push({
+      nome,origin
+  })
 return arr
 }
 
 (async () => {
+  const chunks = chunk(classmem, 250);
   const responses = [];
-
-  for (item in classmem) {
-        const response = await 
-          api
-            .get(classmem[item],{})
-            .then(({ data }) => {
-              return parseLinks(data);
-            })
-        responses.push(response);
-        console.log(item,"/",classmem.length-1)
-        fs.writeFileSync("actor_origin.json",JSON.stringify(responses, null, 2));
+  for (var ck in chunks) {
+    // console.log(chunks[ck])
+    const response = await Promise.all(
+      chunks[ck].map(step =>
+        api
+          .get(step,{})
+          .then(({ data }) => {
+            return parseLinks(data);
+          })
+      )
+    );
+    responses.push(...response);
+    fs.writeFileSync("actor_origin_teste.json", JSON.stringify(responses, null, 2));
+    console.log(`${ck+1}/${chunks.length}`);
   }
 })();
+
+// (async () => {
+//   const responses = [];
+
+//   //Ultimo feito 2760
+//   //Proximo 2761
+//   //Total a serem feitos: 20664
+//   for (item in classmem) {
+//         const response = await 
+//           api
+            // .get(classmem[item],{})
+//             .then(({ data }) => {
+//               return parseLinks(data);
+//             })
+//         responses.push(response);
+//         console.log(item,"/",classmem.length-1)
+//         fs.writeFileSync("actor_origin_teste.json",JSON.stringify(responses, null, 2));
+//   }
+// })();
